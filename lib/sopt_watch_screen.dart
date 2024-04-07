f50a5d2181bc5f94c12e07dc7ae4a1a1eb3381c2
icicle_4 <dwayne.han4@gmail.com>
2024-04-07 22:10:27 +0900

    feat: ListView

--- lib/sopt_watch_screen.dart
+++ lib/sopt_watch_screen.dart
@@ -30,6 +30,17 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
               ),
             ],
           ),
+          SizedBox(
+            width: 100,
+            height: 200,
+            child: ListView(
+              children: const [
+                Center(
+                  child: Text('111'),
+                ),
+              ],
+            ),
+          ),
           const Spacer(),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
